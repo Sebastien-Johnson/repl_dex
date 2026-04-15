917e04f2d7f350280d32110ad57db462becbe685
Sebastien Johnson <sebastien.z.johnson@gmail.com>
2026-04-15 16:41:31 -0400

    mod: 15, ch: 1, les: 3

---
+++ clean_input_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"testing";
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input  string
+		expected []string
+	}{
+		{//test case
+			input: " hello world ",
+			expected: []string{"hello", "wolrd"},
+		},
+		{
+			input: "does this even work?",
+			expected: []string{"does", "this", "even", "work?"},
+		},
+		{
+			input: "extra white space ",
+			expected: []string{"extra", "white", "space"},
+		},
+		//add more cases
+	}
+	//loop over cases and run them all
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				return 
+			}
+		}
+	}
+}
+// func TestSplit(t *testing.T) {
+//     tests := map[string]struct {
+//         input string
+//         sep   string
+//         want  []string
+//     }{
+//         "simple":       {input: "a/b/c", sep: "/", want: []string{"a", "b", "c"}},
+//         "wrong sep":    {input: "a/b/c", sep: ",", want: []string{"a/b/c"}},
+//         "no sep":       {input: "abc", sep: "/", want: []string{"abc"}},
+//         "trailing sep": {input: "a/b/c/", sep: "/", want: []string{"a", "b", "c"}},
+//     }
+
+//     for name, tc := range tests {
+//         t.Run(name, func(t *testing.T) {
+//             got := Split(tc.input, tc.sep)
+//             diff := cmp.Diff(tc.want, got)
+//             if diff != "" {
+//                 t.Fatalf(diff)
+//             }
+//         })
+//     }
