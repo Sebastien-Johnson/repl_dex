c0427043112f1dc6fc3e8a866133e31fe93dc2d8
Sebastien Johnson <sebastien.z.johnson@gmail.com>
2026-04-17 00:21:09 -0400

    Mod15, Ch1, L5

--- main.go
+++ main.go
@@ -1,21 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"bufio"
-	"os"
-)
-
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for ; ; {
-		fmt.Println(("Pokedex > "))
-		scanner.Scan();
-		input := scanner.Text();
-		word := cleanInput(input);
-		response := fmt.Sprintf("Your command was: %s", word[0]);
-		fmt.Println(response)
-	}
+	startRepl()
 }
 
---
+++ repl.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Println(("Pokedex > "))
+		scanner.Scan()
+
+		input := cleanInput(scanner.Text())
+		if len(input) == 0 {
+			continue
+		}
+
+		commandName := input[0]
+
+		command, exists := getCommands()[commandName]
+		if exists { // if exists and checks for error
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else { //if doesnt exist
+			fmt.Println("Unknown command")
+			continue
+		}
+	}
+}
+
+func cleanInput(text string) []string {
+	output := strings.ToLower(text)
+	words := strings.Fields(output)
+	return words
+}
+
+// form struct for command data
+type cliCommand struct {
+	name string
+	description string
+	callback func() error 
+}
+
+//function that returns map that accepts string and returns cli command structs
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name: "exit",
+			description: "Exit the Pokedex",
+			callback: commandExit, //callback
+		},
+		"help": {
+			name: "help",
+			description: "Display a help message",
+			callback: commandHelp, //callback
+		},
+	}
+}
+	
